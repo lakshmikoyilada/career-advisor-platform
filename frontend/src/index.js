07466cd4af75041b033a78d701cc16686ecfd1e0
lakshmikoyilada <lakshmikoyilada1412@gmail.com>
2025-09-21 20:07:28 +0530

    some chages to deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 // src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from 'react-router-dom';
 import App from "./App";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
